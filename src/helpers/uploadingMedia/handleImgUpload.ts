83edf08233efe0364b663082a5caeca357275f46
Firas <FirasDabbabi@gmail.com>
2025-01-27 20:57:57 +0100

    Update handleImgUpload.tsx

--- src/helpers/uploadingMedia/handleImgUpload.ts
+++ src/helpers/uploadingMedia/handleImgUpload.ts
@@ -23,11 +23,11 @@ export default async function handleImgUpload(
         // Upload image from device to Cloudinary and set the image URL and add image to the editor
         const formData = new FormData();
         formData.append('file', uploadedImgFromDevice);
-        formData.append('upload_preset', `${import.meta.env.VITE_UPLOAD_PRESET_KEY}`);
+        formData.append('upload_preset', 'signage');
 
         try {
             const response = await axios.post(
-                `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUD_NAME}/image/upload`,
+                `https://api.cloudinary.com/v1_1/dprqv5quy/image/upload`,
                 formData,
                 {
                     headers: {
